ad85580790defb39af1266da1df760fb919c7864
Marissa Foster <marissa.foster@sinclair.edu>
2021-12-02 13:23:49 -0500

    Define main method and character function and prototype

---
+++ Source.cpp
@@ -0,0 +1,21 @@
+// Marissa Foster
+// CIS1202 - 800
+// Assignment 15a
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+
+int main()
+{
+
+
+}
+
+char character(char start, int offset)
+{
+
+}
