6b6a84c91046e85e3b18515418d84b2821121650
Marissa Foster <marissa.foster@sinclair.edu>
2021-12-02 14:02:54 -0500

    Add try/catch block to character function

--- Source.cpp
+++ Source.cpp
@@ -12,7 +12,11 @@ class invalidCharacterExcpeption
 
 };
 
-class invalidRangeException
+class invalidRangeException 
+{
+
+};
+class caseConversionException
 {
 
 };
@@ -28,7 +32,45 @@ int main()
 char character(char start, int offset)
 {
 	int startAsciiValue = int(start);
-	int tartget = startAsciiValue + offset;
+	int target = startAsciiValue + offset;
 
+    try {
+        //if start is not lower case letter or upper case letter throw invalidCharacterExcpeption
+        if (startAsciiValue < 65 || (startAsciiValue > 90 && startAsciiValue < 97) || startAsciiValue > 122)
+        {
+            throw invalidCharacterExcpeption();
+        }
+
+        //else if target is not lower case letter or upper case letter throw invalidRangeException
+        else if (target < 65 || (target > 90 && target < 97) || target > 122)
+        {
+            throw invalidRangeException();
+        }
+
+        //else if target is lower case and start is upper case or target is upper case and start is lower case then throw caseConversionException
+        else if ((target >= 65 && target <= 90 && startAsciiValue >= 95 && startAsciiValue <= 122) || (target >= 95 && target <= 122 && startAsciiValue >= 65 && startAsciiValue <= 90))
+        {
+            throw caseConversionException();
+        }
+    }
+
+    catch (invalidCharacterExcpeption exception1)
+    {
+        cout << " Invalid Character Exception " << endl;
+        return ' ';
+    }
+    catch (invalidRangeException exception2)
+    {
+        cout << " Invalid Range Exception " << endl;
+        return ' ';
+    }
+    catch (caseConversionException exception3)
+    {
+        cout << " Cannot make case changes from upper to lower or otherwise " << endl;
+        return ' ';
+    }
+
+    //if no exception was catched return the character value of target
+    return char(target);
 
 }
